b58d7066e539ae3fd409aba7ca83b830a87905b2
Argan <argan.piquet@gmail.com>
2019-12-14 23:58:24 +0100

    initial commit

--- tests/model/Model.php
+++ tests/model/Model.php
@@ -1,15 +1,50 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: argan
- * Date: 14/12/2019
- * Time: 22:51
- */
 
 namespace App\Tests;
 
+require_once('../../core/Uploader.php');
+require_once('../../libs/Logs.php');
+
+use App\Core\Uploader;
+use App\Libs\Logs;
 
 class Model
 {
+    public function uploadFile()
+    {
+        try {
+            $uploader = new Uploader();
+            $filename = $uploader->getFileName();
+            $ext = $uploader->getFileExtension($_FILES['myFile']);
+            $uploaded = $uploader->upload($_FILES['myFile'], $filename, '/uploader/tests/images/', 200, 200);
+        } catch (\Exception $e) {
+            $logs = new Logs();
+            $logs->write('An error occured during file upload.', $e);
+        }
 
+        switch ($uploaded)
+        {
+            case $uploader::FILE_SAVED:
+                // Success
+                // Do something...
+                echo 'Image successfully uploaded';
+                break;
+            case $uploader::ERROR_FILE_HEAVY:
+                // File heavy
+                echo 'Your file is heavy.';
+                break;
+            case $uploader::FILE_UPLOAD_ERROR:
+                // An error occurred during file upload
+                echo 'An error occurred.. Please, try again';
+                break;
+            case $uploader::FILE_RESIZE_ERROR:
+                // An error occured during file resizing.
+                echo 'An error occurred.. Please, try again';
+                break;
+        }
+    }
 }
+
+$m = new Model();
+$m->uploadFile();
+//header('Location:/uploader/tests/index.html');
